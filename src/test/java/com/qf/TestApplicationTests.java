155ae6d42e7d114ddbb1a3519d65e54b9bbbd90e
何朋飞 <757217380@qq.com>
2022-03-27 13:17:44 +0800

    首次提交

---
+++ src/test/java/com/qf/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qf;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
